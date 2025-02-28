e693c6ae4b8e3636411c7d7e3bd8f8612562ea55
MystiaFin <cytrsx01@gmail.com>
2025-02-28 17:29:14 +0700

    update bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,3 +1,22 @@
-return (
-  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'}
-)
+return {
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("bufferline").setup({
+			highlights = {
+				fill = { bg = "#191724" }, -- Darker base from Rosé Pine
+				background = { fg = "#e0def4", bg = "#1f1d2e" },
+				buffer_selected = { fg = "#c4a7e7", bold = true },
+				separator = { fg = "#1f1d2e", bg = "#191724" },
+				separator_selected = { fg = "#1f1d2e" },
+				indicator_selected = { fg = "#c4a7e7", bg = "#1f1d2e" },
+			},
+			options = {
+				separator_style = "slant",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				always_show_bufferline = true,
+			},
+		})
+	end,
+}
--- lua/plugins/rosepine.lua
+++ lua/plugins/rosepine.lua
@@ -3,6 +3,14 @@ return {
 	event = "VimEnter",
 	name = "rose-pine",
 	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+		vim.cmd("colorscheme rose-pine-moon")
+		require("rose-pine").setup({
+			variant = "moon", -- Choose between 'main', 'moon', or 'dawn'
+			highlight_groups = {
+				BufferLineFill = { bg = "base" },
+				BufferLineBackground = { fg = "muted", bg = "surface" },
+				BufferLineBufferSelected = { fg = "text", bg = "overlay", bold = true },
+			},
+		})
+	end,
 }
